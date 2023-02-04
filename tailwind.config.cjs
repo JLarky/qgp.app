77f15a2a1ff06993936abb961afc8b7f577e9534
Jesse Pence <jessepence@gmail.com>
2023-02-03 23:43:52 -0700

    new website design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					DARK: '#0c1e11',
+					NOTSODARK: '#14321e',
+					MEDIUM: '#22c55e',
+					NOTSOLIGHT: '#E5F9E8',
+					LIGHT: '#C3EFCE',
+				},
+				secondary: {
+					DARK: '#1e0c0c',
+					NOTSODARK: '#3e1e1e',
+					MEDIUM: '#ff3f34',
+					NOTSOLIGHT: '#F9E5E5',
+					LIGHT: '#FCE3E3',
+				},
+			},
+		},
 	},
 	plugins: [],
 };
