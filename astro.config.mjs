5cdc0ee83a92c913022469d80d8829660e8e66ca
JLarky <jlarky@gmail.com>
2023-02-09 22:14:05 -0700

    yarn astro add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
+import partytown from '@astrojs/partytown';
 import { defineAstro } from 'qgp';
 
 import { common } from './qgp.config.mjs';
@@ -10,7 +11,7 @@ import { common } from './qgp.config.mjs';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://qgp.app',
-	integrations: [mdx(), sitemap(), react()],
+	integrations: [mdx(), sitemap(), react(), partytown()],
 	vite: defineAstro(common, {}),
 	server: { port: 3000 },
 });
